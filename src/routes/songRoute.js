3839cb6d91490becfd92b66e9bca4e5ace033038
darshan-devani-777 <darshan.devani@tecocraft.com>
2025-07-14 16:45:19 +0530

    Music-Player

--- src/routes/songRoute.js
+++ src/routes/songRoute.js
@@ -32,8 +32,8 @@ router.get("/song/get-all-song", authMiddleware , controller.getAllSongs);
 // GET NEW RELEASED SONGS (Admin / User)
 router.get("/song/new-released-song", authMiddleware , controller.getNewReleasedSongs);
 
-// GET SPECIFIC SONG (Admin)
-router.get("/song/get-specific-song/:id", authMiddleware , isAdmin , controller.getSongById);
+// GET SPECIFIC SONG (Admin / User)
+router.get("/song/get-specific-song/:id", authMiddleware , controller.getSongById);
 
 // UPDATE SONG (Admin)
 router.put("/song/update-song/:id",  upload.array("songImage") , authMiddleware , isAdmin , validateUpdateSong , validate , controller.updateSong);
